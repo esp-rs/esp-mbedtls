a94eb9f428d3b4b24d61c5746742b4db33767fdb
ivmarkov <ivan.markov@gmail.com>
2026-09-06 16:19:50 +0000

    Use an alternative mTLS server

--- examples/std/src/bin/client.rs
+++ examples/std/src/bin/client.rs
@@ -1,6 +1,6 @@
 //! Example of a client connection to a server, using the async API.
 //!
-//! This example connects to `https://httpbin.org/ip` and then to `https://certauth.cryptomix.com/json/` (mTLS)
+//! This example connects to `https://httpbin.org/ip` and then to `https://client.badssl.com/` (mTLS)
 //! and performs a simple HTTPS 1.0 GET request to each.
 
 use std::net::{TcpStream, ToSocketAddrs};
@@ -41,7 +41,7 @@ async fn run(buf: &mut [u8]) {
 
     for (index, (server_name_cstr, server_path, mtls)) in [
         (c"httpbin.org", "/ip", false),
-        (c"certauth.cryptomix.com", "/json/", true),
+        (c"client.badssl.com", "/", true),
     ]
     .into_iter()
     .enumerate()
